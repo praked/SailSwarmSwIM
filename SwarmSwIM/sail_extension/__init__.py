2165b12cc326e500f2cd287e60775e1860afd4f1
Pranav Kedia <pranavkedia11@gmail.com>
2025-11-14 12:11:18 +0100

    Create __init__.py

---
+++ SwarmSwIM/sail_extension/__init__.py
@@ -0,0 +1,6 @@
+"""
+Sailing extension for SwarmSwIM.
+
+This package provides sailing physics, navigation, plotting, and agent
+implementations (e.g. SailAgent, WindPlotter, TackingNavigation).
+"""
